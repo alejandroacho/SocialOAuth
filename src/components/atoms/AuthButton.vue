1e614c42b5df6c6b2bae2682d71bc8dfa031fde0
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-18 21:19:34 +0200

    Add atom for the login/signin button

---
+++ src/components/atoms/AuthButton.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: ['method', 'company'],
+  methods: {
+    capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+  },
+  created() {
+    this.src = `src/assets/${this.company}.svg`;
+  }
+}
+</script>
+
+<template>
+  <button :id="company">
+    <img :src="src" class="white-svg logo"/>
+    <p>{{capitalize(method)}} with {{capitalize(company)}}</p>
+  </button>
+</template>
