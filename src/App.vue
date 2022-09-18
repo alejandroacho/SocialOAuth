834d8749cc82b681be4917d92a3bee3427756d31
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-18 21:20:14 +0200

    Add molecule to create the buttons for social oauth

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script setup>
+import SocialAuth from './components/molecules/SocialAuth.vue'
 </script>
 
 <style scoped>
---
+++ src/components/molecules/SocialAuth.vue
@@ -0,0 +1,49 @@
+<script>
+import AuthButton from '../atoms/AuthButton.vue';
+
+export default {
+  name: 'SocialAuth',
+  components: {
+    AuthButton,
+  },
+  data() {
+    return {
+      method: "login",
+    };
+  },
+  methods: {
+    changeMethod() {
+      this.method = this.otherMethod;
+    },
+  },
+  computed: {
+    otherMethod() {
+      return this.method === 'login' ? 'sign up' : 'login';
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Social OAuth</h1>
+  <hr>
+  <div class="social-auth">
+    <AuthButton :method="method" company="google"/>
+    <AuthButton :method="method" company="facebook"/>
+    <AuthButton :method="method" company="twitter"/>
+  </div>
+  <br>
+  <a v-on:click="changeMethod">Or {{otherMethod}}</a>
+</template>
+
+<style scoped>
+  hr{
+    margin-bottom: 50px
+  }
+  .social-auth{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+</style>
